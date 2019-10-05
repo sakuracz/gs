e2982a9fd4502fb40dbc9c071c9da1f10a793b3d
sakuracz <memozidozu.rinkimirikata@gmail.com>
2019-10-05 17:12:38 +0200

    adds tests and implementation for file open

--- parser.hpp
+++ parser.hpp
@@ -1,5 +1,30 @@
-class parser
+#include <string>
+#include <iostream>
+#include <time.h>
+#include <fstream>
+
+using std::string;
+using std::ifstream;
+
+class IParser
 {
 public:
-    parser(){};
+   virtual bool openTickFile(string) = 0;
+   virtual void print(time_t, time_t, string) = 0;
+   virtual void product(time_t, time_t, string, string, string) = 0;
+};
+
+
+class Parser : public IParser
+{
+public:
+    bool openTickFile(string fName) override
+    {
+        data = ifstream(fName, std::ios::in);
+        return data.is_open();
+    }
+    void print(time_t startTime, time_t endTime, string symbol) override {};
+    void product(time_t startTime, time_t endTime, string symbol, string field1, string field2) override{};
+private:
+    ifstream data;
 };
--- tests.cpp
+++ tests.cpp
@@ -1,20 +1,56 @@
 #include <gtest/gtest.h>
 #include "parser.hpp"
+#include <string>
+#include <fstream>
 
+using namespace std;
 using namespace testing;
 
-class parserTestSuite : public Test
+class IParserTestSuite
 {
+private:
+    virtual void generateInputFile() = 0;
 };
 
-TEST_F(parserTestSuite, dummy)
+class SmallFileParserTestSuite : public IParserTestSuite, public Test
+{
+private:
+    void generateInputFile() override
+    {
+        if(createFile())
+        {
+            outFile.close();
+        };
+    };
+    std::string fileName{"herpDerp.dat"};
+    bool createFile()
+    {
+        outFile = ofstream(fileName, ios::out);
+        outFile.is_open();
+    };
+    ofstream outFile;
+};
+
+TEST_F(SmallFileParserTestSuite, dummy)
 {
     ASSERT_TRUE(true);
 }
 
-TEST_F(parserTestSuite, createObject)
+TEST_F(SmallFileParserTestSuite, createObject)
 {
-    parser sut{};
+    Parser sut{};
+}
+
+TEST_F(SmallFileParserTestSuite, returnsFalseWhileTryingToOpenNonexistingFile)
+{
+    Parser sut{};
+    ASSERT_FALSE(sut.openTickFile("herpderp"));
+}
+
+TEST_F(SmallFileParserTestSuite, returnsTrueWhilstOpeningEmptyFile)
+{
+    Parser sut{};
+    ASSERT_TRUE(sut.openTickFile("herpderp"));
 }
 
 int main(int argc, char **argv)
